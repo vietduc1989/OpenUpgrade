471feb0a3008c18329785624061b5e7742c29c0f
Giang Pham <phamgiang2510@gmail.com>
2022-06-09 10:06:09 +0700

    [IMP+MIG][12.0] hr: Clear the default employee's certificate of Master

--- addons/hr/migrations/12.0.1.1/post-migration.py
+++ addons/hr/migrations/12.0.1.1/post-migration.py
@@ -16,9 +16,19 @@ def fill_hr_employee_resource_calendar_and_user(cr):
     )
 
 
+def clear_hr_employee_default_certificate(cr):
+    openupgrade.logged_query(
+        cr, """
+        UPDATE hr_employee
+        SET certificate = NULL
+        """
+    )
+
+
 @openupgrade.migrate(use_env=False)
 def migrate(cr, version):
     fill_hr_employee_resource_calendar_and_user(cr)
+    clear_hr_employee_default_certificate(cr)
     openupgrade.load_data(
         cr, 'hr', 'migrations/12.0.1.1/noupdate_changes.xml')
     openupgrade.delete_record_translations(
