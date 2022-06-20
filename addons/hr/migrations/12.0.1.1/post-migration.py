68ed61b92ae8da7adcf4f5944b082d1671d134b5
Đặng Thành Luân <65998903+thanhluan240287@users.noreply.github.com>
2022-06-20 10:56:20 +0700

    [MIG+IMP][12.0] hr: Fill job titles for employees (#251)

--- addons/hr/migrations/12.0.1.1/post-migration.py
+++ addons/hr/migrations/12.0.1.1/post-migration.py
@@ -15,6 +15,15 @@ def fill_hr_employee_resource_calendar_and_user(cr):
         """
     )
 
+def fill_hr_employee_job_title(cr):
+    openupgrade.logged_query(
+        cr, """
+        UPDATE hr_employee he
+        SET job_title = hj.name
+        FROM hr_job hj
+        WHERE he.job_id = hj.id
+        """
+    )
 
 def clear_hr_employee_default_certificate(cr):
     openupgrade.logged_query(
@@ -29,6 +38,7 @@ def clear_hr_employee_default_certificate(cr):
 def migrate(cr, version):
     fill_hr_employee_resource_calendar_and_user(cr)
     clear_hr_employee_default_certificate(cr)
+    fill_hr_employee_job_title(cr)
     openupgrade.load_data(
         cr, 'hr', 'migrations/12.0.1.1/noupdate_changes.xml')
     openupgrade.delete_record_translations(
